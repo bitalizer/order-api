93e97a8baf9858120894ff986ae39843fc4e4af1
Bitalizer <hungryjesus@gmail.com>
2023-09-25 15:37:46 +0300

    docs: Add comments to OrderService interface

--- src/main/java/com/example/order/service/OrderService.java
+++ src/main/java/com/example/order/service/OrderService.java
@@ -8,8 +8,18 @@ import org.springframework.data.jpa.domain.Specification;
 
 public interface OrderService {
 
+	/**
+	 * Creates a new order based on the provided order request.
+	 * @param orderRequest The order request containing customer and product details.
+	 * @return An order response representing the newly created order.
+	 */
 	OrderResponse createOrder(OrderRequest orderRequest);
 
+	/**
+	 * Retrieves a list of filtered orders based on the provided specification.
+	 * @param spec The specification used to filter orders.
+	 * @return A list of order responses representing the filtered orders.
+	 */
 	List<OrderResponse> getFilteredOrders(Specification<Order> spec);
 
 }
