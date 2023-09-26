2a366c38a833dbcd6cfc71efdae9e5dd114040fc
Bitalizer <hungryjesus@gmail.com>
2023-09-26 14:45:08 +0300

    test: update OrderServiceTest to Adapt to Modified Service Class
    Refactored the OrderServiceTest class to accommodate changes in the OrderServiceImpl service class. Updated test cases to ensure the service functions correctly with the latest changes.

--- src/test/java/com/example/order/service/OrderServiceTest.java
+++ src/test/java/com/example/order/service/OrderServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.example.order.domain.dto.request.OrderLineRequest;
@@ -13,69 +15,90 @@ import com.example.order.domain.dto.response.OrderResponse;
 import com.example.order.domain.model.Customer;
 import com.example.order.domain.model.Order;
 import com.example.order.domain.model.Product;
+import com.example.order.exception.CustomerNotFoundException;
+import com.example.order.exception.ProductNotFoundException;
+import com.example.order.repository.CustomerRepository;
 import com.example.order.repository.OrderRepository;
-import com.example.order.service.impl.OrderServiceImpl;
+import com.example.order.repository.ProductRepository;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class OrderServiceTest {
 
-	@Mock
+	private static Customer customer;
+
+	private static Product product;
+
+	private static Order order;
+
+	@Autowired
+	private OrderService orderService;
+
+	@MockBean
 	private OrderRepository orderRepository;
 
-	@Mock
-	private ProductService productService;
+	@MockBean
+	private CustomerRepository customerRepository;
 
-	@Mock
-	private CustomerService customerService;
+	@MockBean
+	private ProductRepository productRepository;
 
-	@Mock
-	private ModelMapper modelMapper;
+	@BeforeAll
+	public static void setUp() {
+		Long customerId = 1L;
+		Long productId = 5L;
+		Long orderId = 50L;
 
-	@InjectMocks
-	private OrderServiceImpl orderService;
+		customer = new Customer();
+		customer.setId(customerId);
+
+		product = new Product();
+		product.setId(productId);
+		product.setName("Dummy Product");
+		product.setSkuCode("SKU123");
+		product.setUnitPrice(new BigDecimal("123.45"));
+
+		order = new Order();
+		order.setId(orderId);
+		order.setCustomer(customer);
+		order.addOrderLine(product, 10);
+	}
 
 	@Test
 	void should_CreateOrder_When_ValidRequest() {
 
-		Long customerId = 1L;
-		Long orderId = 50L;
-
-		Set<OrderLineRequest> orderLines = new HashSet<>();
-		orderLines.add(new OrderLineRequest(10, 1L));
-		Customer customer = new Customer();
-		customer.setId(customerId);
-		Product product = new Product();
-		product.setId(1L);
+		Set<OrderLineRequest> orderLineRequests = new HashSet<>();
+		orderLineRequests.add(new OrderLineRequest(10, product.getId()));
 
 		OrderRequest orderRequest = new OrderRequest();
-		orderRequest.setCustomerId(customerId);
-		orderRequest.setOrderLines(orderLines);
+		orderRequest.setCustomerId(customer.getId());
+		orderRequest.setOrderLines(orderLineRequests);
 
-		OrderResponse orderResponse = new OrderResponse();
-		orderResponse.setCustomerId(customerId);
-		orderResponse.setOrderId(orderId);
+		when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
+		when(productRepository.findAllById(anySet())).thenReturn(Collections.singletonList(product));
+		when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-		when(customerService.getCustomerById(customerId)).thenReturn(customer);
-		when(productService.getProductsById(anySet())).thenReturn(Collections.singletonList(product));
-		when(orderRepository.save(any(Order.class))).thenReturn(new Order());
-		when(modelMapper.map(any(), any())).thenReturn(orderResponse);
-
-		OrderResponse orderResponseActual = orderService.createOrder(orderRequest);
+		OrderResponse orderResponse = orderService.createOrder(orderRequest);
 
 		assertNotNull(orderResponse);
-		assertEquals(customerId, orderResponseActual.getCustomerId());
-		assertEquals(orderId, orderResponseActual.getOrderId());
+		assertNotNull(orderResponse.getOrderLines());
+		assertEquals(customer.getId(), orderResponse.getCustomerId());
+		assertEquals(orderLineRequests.size(), orderResponse.getOrderLines().size());
+		assertEquals(order.getId(), orderResponse.getOrderId());
+
+		// Verify that the methods were called the expected number of times
+		verify(customerRepository, times(1)).findById(customer.getId());
+		verify(productRepository, times(1)).findAllById(anySet());
+		verify(orderRepository, times(1)).save(any(Order.class));
 	}
 
 	@Test
@@ -86,35 +109,25 @@ class OrderServiceTest {
 		OrderRequest orderRequest = new OrderRequest();
 		orderRequest.setCustomerId(customerId);
 
-		when(customerService.getCustomerById(customerId))
-			.thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Customer not found"));
-
-		ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
-			orderService.createOrder(orderRequest);
-		});
-
-		assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+		assertThrows(CustomerNotFoundException.class, () -> orderService.createOrder(orderRequest));
+		verify(orderRepository, times(0)).save(any(Order.class));
 	}
 
 	@Test
 	void should_FailToCreateOrder_When_OrderLineProductIdNotExists() {
-		Long customerId = 1L;
 		Set<OrderLineRequest> orderLines = new HashSet<>();
-		orderLines.add(new OrderLineRequest(10, 1L)); // Product ID that does not exist
-		Customer customer = new Customer();
-		customer.setId(customerId);
+		orderLines.add(new OrderLineRequest(10, 1L));
 
 		OrderRequest orderRequest = new OrderRequest();
-		orderRequest.setCustomerId(customerId);
+		orderRequest.setCustomerId(customer.getId());
 		orderRequest.setOrderLines(orderLines);
 
-		when(customerService.getCustomerById(customerId)).thenReturn(customer);
-		when(productService.getProductsById(anySet()))
-			.thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product not found"));
+		when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
+		when(productRepository.findAllById(anySet())).thenReturn(Collections.emptyList());
 
-		assertThrows(ResponseStatusException.class, () -> {
-			orderService.createOrder(orderRequest);
-		});
+		assertThrows(ProductNotFoundException.class, () -> orderService.createOrder(orderRequest));
+
+		verify(orderRepository, times(0)).save(any(Order.class));
 	}
 
 }
